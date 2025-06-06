6a5c384f41bf1742d890d245f5c729a8fa41a238
vsaxena12 <36344027+vsaxena12@users.noreply.github.com>
2025-06-05 22:16:33 -0500

    Initializa Health Monitor

---
+++ healthMonitor/src/main/java/com/hm/healthMonitor/HealthMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.hm.healthMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMonitorApplication.class, args);
+	}
+
+}
---
+++ healthMonitor/src/test/java/com/hm/healthMonitor/HealthMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.healthMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
