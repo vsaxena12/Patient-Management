bf3d25ecdf8287f75f7fbc8c4d051dcda57800d0
vsaxena12 <36344027+vsaxena12@users.noreply.github.com>
2026-06-19 16:41:49 -0500

    Renaming to patientService

---
+++ patientService/src/main/java/com/hm/healthMonitor/PatientServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hm.healthMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+}
